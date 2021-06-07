dfac9a04b519db9f08f2ec2c5fef32f18cb8a233
Marbett Hedderich <marbett@gmail.com>
2021-06-07 16:19:39 +0200

    pipes de angular y primeNg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  nombre: string = 'MarBett Hedderich';
-  valor: number = 1000;
-  obj = {
-    nombre: "Ana Sofia"
+export class AppComponent implements OnInit {
+  
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
   }
 
-  mostrarNombre () {
-    console.log(this.nombre);
-    console.log(this.valor);
-    console.log(this.obj);
-  }
 }
